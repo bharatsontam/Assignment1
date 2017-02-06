bb73cfeaeeca232f0e702243f2e0b93f825e9485
Bharat Sontam <sontambharathkumar@hotmail.com>
2017-02-06 17:38:55 -0600

    Assignment first commit

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std1 = new Student();
+            std1.FirstName = "Bharath";
+            std1.LastName = "Sontam";
+
+            Student std2 = new Student();
+            std2.FirstName = "Sarath";
+            std2.LastName = "Sontam";
+
+            Console.WriteLine("Studnet 1 fullname: {0}", std1.GetFullName());
+            Console.WriteLine("Studnet 2 fullname: {0}", std2.GetFullName());
+            Console.WriteLine("No.of students: {0}", Student.Count);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Student
+    {
+        public static int Count = 0;
+
+        public Student()
+        {
+            Count++;
+        }
+
+        public string FirstName;
+        public string LastName;
+
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+    }
+}
